bfae52193f7a3fd6c1f7658aa47f4b2f59c02349
RenerRamirez <renerramirez@github.com>
2024-04-11 21:09:48 +0800

    Add timerfd and (latest) key pressed

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::io::{stdin, stdout, Write};
+use std::time::Duration;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use timerfd::{TimerFd, TimerState, SetTimeFlags};
 
 fn main() {
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
+    let mut tfd = TimerFd::new().unwrap();
 
     let mut i = 1;
     write!(stdout, "{}", termion::clear::All).unwrap();
@@ -13,9 +16,25 @@ fn main() {
     stdout.flush().unwrap();
 
     // each key press fills up the .keys() "buffer"
+    tfd.set_state(TimerState::Oneshot(Duration::new(0, 500)), SetTimeFlags::Default);
     for c in stdin.keys() {
+        write!(stdout, "{}", termion::cursor::Goto(1, 1),).unwrap();
+        stdout.flush().unwrap();
+
+        print!("Keys pressed: [ ");
+        match c {
+            Ok(Key::Char(k)) => print!("{k}"),
+            _ => (),
+        }
+        println!(" ] ");
+        
+        match tfd.get_state() {
+            TimerState::Oneshot(d) => println!("Remaining: {:?}", d),
+            _ => (),
+        }
+
         // move cursor to top-left of the terminal 
-        write!(stdout, "{}", termion::cursor::Goto(i, 1),).unwrap();
+        write!(stdout, "{}", termion::cursor::Goto(i, 5),).unwrap();
 
         i += 1;
 
@@ -37,12 +56,12 @@ fn main() {
                 // clear whole screen
                 i = 1;
                 write!(stdout, "{}", termion::clear::All,).unwrap();
-                write!(stdout, "{}", termion::cursor::Goto(i, 1)).unwrap();
+                write!(stdout, "{}", termion::cursor::Goto(i, 5)).unwrap();
             }
             Key::Backspace => {
                 i = 1;
                 write!(stdout, "{}", termion::clear::All,).unwrap();
-                write!(stdout, "{}", termion::cursor::Goto(i, 1)).unwrap();
+                write!(stdout, "{}", termion::cursor::Goto(i, 5)).unwrap();
             }
             _ => (),
         }
